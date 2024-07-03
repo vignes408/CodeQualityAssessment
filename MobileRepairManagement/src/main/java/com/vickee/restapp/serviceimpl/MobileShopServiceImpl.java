45c8334e5b43f874be445b719200c6b4904dc100
VIGNESWARAN RK <Vignesrajesh02@gmail.com>
2024-07-03 15:11:49 +0530

    Files were added

---
+++ MobileRepairManagement/src/main/java/com/vickee/restapp/serviceimpl/MobileShopServiceImpl.java
@@ -0,0 +1,48 @@
+package com.vickee.restapp.serviceimpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vickee.restapp.model.MobileShop;
+import com.vickee.restapp.repository.MobileShopRepo;
+import com.vickee.restapp.service.MobileShopService;
+
+@Service
+public class MobileShopServiceImpl implements MobileShopService {
+
+	
+	@Autowired
+	MobileShopRepo repo;
+	
+	@Override
+	public void addMobileShop(MobileShop shop) {
+		repo.save(shop);
+	}
+
+	@Override
+	public MobileShop getMobileShop(int mid) {
+	    Optional<MobileShop> mobileOptional = repo.findById(mid);
+	    return mobileOptional.orElse(null);
+	}
+	
+	@Override
+	public List<MobileShop> getAllMobileShop() {
+		return repo.findAll();
+	}
+
+	@Override
+	public void updateMobileShop(MobileShop shop) {
+		repo.save(shop);
+		
+	}
+
+	@Override
+	public void deleteMobileShop(int mid) {
+		repo.deleteById(mid);
+		
+	}
+
+}
